4d05bfccff417b749cf37d122961e3e8f6566e92
JMCromp <jordancrompton@hotmail.com>
2022-07-15 11:01:03 +0100

    added objectsExample.js

---
+++ objectsExample.js
@@ -0,0 +1,34 @@
+// const person = {
+//     _name: "Jordan",
+//     age: 31,
+//     movies: ["A New Hope", "Anything from Marvel"],
+//     name() {
+//         return _name;
+//     }
+// };
+
+// console.log(person.name());
+
+const person = {
+    name: "Hamza",
+    age: 18,
+    movies: ["A New Hope", "Avengers", "Dark Knight"],
+    sayHi() {
+        return `Hello, my name is ${this.name}`;
+    }
+};
+
+console.log(person.sayHi())
+console.log(`I am ${person.age} and my Top 3 movies are:\n${person.movies[0]}\n${person.movies[1]}\n${person.movies[2]}`)
+
+// let array = [
+//     ["water", 1], ["pepsi", 1.50], ["coke", 1.75]
+// ];
+
+// console.log(array[2][1]);
+
+// if (number & 1) {
+//     console.log("Odd")
+// } else {
+//     console.log("Even")
+// }
