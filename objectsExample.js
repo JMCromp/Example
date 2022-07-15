360fdde583f2e6b29013dd51369a4f5fe33b4421
JMCromp <jordancrompton@hotmail.com>
2022-07-15 11:02:40 +0100

    edited ObjectsExample.js

--- objectsExample.js
+++ objectsExample.js
@@ -9,17 +9,17 @@
 
 // console.log(person.name());
 
-const person = {
-    name: "Hamza",
-    age: 18,
-    movies: ["A New Hope", "Avengers", "Dark Knight"],
-    sayHi() {
-        return `Hello, my name is ${this.name}`;
-    }
-};
+// const person = {
+//     name: "Hamza",
+//     age: 18,
+//     movies: ["A New Hope", "Avengers", "Dark Knight"],
+//     sayHi() {
+//         return `Hello, my name is ${this.name}`;
+//     }
+// };
 
-console.log(person.sayHi())
-console.log(`I am ${person.age} and my Top 3 movies are:\n${person.movies[0]}\n${person.movies[1]}\n${person.movies[2]}`)
+// console.log(person.sayHi())
+// console.log(`I am ${person.age} and my Top 3 movies are:\n${person.movies[0]}\n${person.movies[1]}\n${person.movies[2]}`)
 
 // let array = [
 //     ["water", 1], ["pepsi", 1.50], ["coke", 1.75]
